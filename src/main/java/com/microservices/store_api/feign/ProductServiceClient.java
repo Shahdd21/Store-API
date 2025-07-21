053bdc13e0aa5e7e33d162e66b99dc5a5909e20b
Shahdd21 <88803131+Shahdd21@users.noreply.github.com>
2025-07-22 01:32:12 +0300

    added Catalog Service remote url

--- src/main/java/com/microservices/store_api/feign/ProductServiceClient.java
+++ src/main/java/com/microservices/store_api/feign/ProductServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient("PRODUCTSERVICE")
+@FeignClient(value = "PRODUCTSERVICE", url = "${product.service.url}")
 public interface ProductServiceClient {
 
     @PostMapping("/products/stores")
